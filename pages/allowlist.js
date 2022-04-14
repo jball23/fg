5398bca424cdb899b667278e713adae62d42f20e
Jordan Ballard <ballard.jordan@gmail.com>
2022-04-14 13:18:37 -0400

    Create basic AllowList component

--- pages/allowlist.js
+++ pages/allowlist.js
@@ -1,33 +1,17 @@
-// import Head from "next/head";
+import Head from "next/head";
 // import { useMoralis } from "react-moralis";
-// import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.css";
 
-// export default function Allowlist() {
-//   const { authenticate, isAuthenticated, logout, user } = useMoralis();
+export default function Allowlist() {
+  // const { authenticate, isAuthenticated, logout, user } = useMoralis();
 
-//   return (
-//     <div className={styles.container}>
-//       <Head>
-//         <title>Allow List for Fantasy Garage E30 M3</title>
-//         <meta name="description" content="The OG NFT project for car enthusiasts." />
-//         <link rel="icon" href="/favicon.ico" />
-//       </Head>
-
-//       {isAuthenticated ? (
-//         <>
-//           <button onClick={logout} className="bg-white">Logout</button>
-//           <h2>Welcome {user.get("username")}</h2>
-//           <h2>Your wallet address is {user.get("ethAddress")}</h2>
-//         </>
-//       ) : (
-//         <button
-//           onClick={() => {
-//             authenticate({ provider: "metamask" });
-//           }}
-//         >
-//           Sign in with MetaMask
-//         </button>
-//       )}
-//     </div>
-//   );
-// };
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Allow List for Fantasy Garage E30 M3</title>
+        <meta name="description" content="The OG NFT project for car enthusiasts." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  );
+};
