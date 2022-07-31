484cc3237eb922c6701256a1c427edee89439c17
Jordan Ballard <ballard.jordan@gmail.com>
2022-07-30 20:02:30 -0400

    Update font weight and spacing for CTA

--- components/Cta.js
+++ components/Cta.js
@@ -13,10 +13,10 @@ export const Cta = ({ image, heading, subHeading, buttonText }) => {
         }
         <div className="block md:mr-12 mb-4 md:mb-0">
           <p className="font-sans text-3xs tracking-widest font-medium uppercase opacity-60">{heading}</p>
-          <p className="font-sans uppercase tabular-nums font-bold md:text-xl text-sm">{subHeading}</p>
+          <p className="font-sans uppercase tabular-nums font-bold md:text-xl text-sm tracking-wider">{subHeading}</p>
         </div>
         <div className="md:ml-auto w-full md:w-auto">
-          <a className="lg:px-6 py-3 px-4 group flex items-center justify-center w-full shadow-lg lg:text-base text-2xs uppercase duration-300 cursor-pointer font-black transform text-fg-pink bg-white" href="https://opensea.io/collection/beanzofficial" target="_blank" rel="noreferrer">
+          <a className="lg:px-6 py-3 px-4 group flex items-center justify-center w-full shadow-lg lg:text-base text-2xs uppercase duration-300 cursor-pointer font-bold tracking-wider transform text-fg-pink bg-white" href="https://opensea.io/collection/beanzofficial" target="_blank" rel="noreferrer">
             <span className="block lg:hidden">{buttonText}</span>
             <span className="hidden lg:block">{buttonText}</span>
             <span className="hidden lg:block pl-4 duration-300 transform translate-x-0 lg:group-hover:translate-x-2">
