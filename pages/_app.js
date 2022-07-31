ddfecc76cb4fb82e40a8522b2ded0ed8e50a1504
Jordan Ballard <ballard.jordan@gmail.com>
2022-07-30 20:01:43 -0400

    Add GA tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
 // import { MoralisProvider } from "react-moralis";
+import ReactGA from 'react-ga';
+import React from 'react';
 import "../styles/globals.css";
 import "../styles/animation.scss";
 
 function MyApp({ Component, pageProps }) {
+  React.useEffect(() => {
+    ReactGA.initialize(`${process.env.GA_TRACKING_ID}`)
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
+
   return (
     // <MoralisProvider
     //   appId={process.env.NEXT_PUBLIC_MORALIS_APP_ID}
